c983b01939394f2979f39754789ecd28b90acac3
JonathanEnright <123285464+JonathanEnright@users.noreply.github.com>
2025-03-17 18:48:38 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -252,15 +252,16 @@ blank1, main_title, blank2 = st.columns([2,5,2])
 top1, gap, main_gap, gap2, top3 = st.columns([1,1,5,1,1])
 
 with top1:
-    with st.container(border=True):
+    with st.container(height=100,border=True):
         st.write("Last Updated: Today!")
     # st.button("Last Updated: Today!", use_container_width=True, disabled=True)
 with main_gap:
-    with st.container(border=True):
+    with st.container(height=100,border=True):
             st.title("Age of Empires 2 Analysis")
     # st.button("Age of Empires 2 Analysis", use_container_width=True)
 with top3:
-    st.button("Reset All filters", on_click=reset_state_callback, use_container_width=True)
+    with st.container(height=100,border=False):
+        st.button("Reset All filters", on_click=reset_state_callback, use_container_width=True)
 
 
 
